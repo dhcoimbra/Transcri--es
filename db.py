70d58ad0c1a799c49238857f999b158161af438e
Daniel Coimbra <danielhcoimbra@hotmail.com>
2024-11-04 13:30:54 -0300

    Alterações Coimbra2

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
-#import psycopg2
+import psycopg2
 import os
-import sqlite3
+
 # Função para conectar ao banco de dados PostgreSQL
-"""def conectar():
+def conectar():
     try:
         conn = psycopg2.connect(
             host=os.getenv('DB_HOST', 'localhost'),
@@ -16,71 +16,70 @@ import sqlite3
     except psycopg2.Error as e:
         print(f"Erro ao conectar ao banco de dados: {e}")
         raise
-"""
+
 
 # Função para criar a tabela de transcrições
 def criar_tabela_transcricoes():
     try:
-        #conn = conectar()
-        conn = conn = sqlite3.connect('transcricoes_db.db')
-
+        conn = conectar()
         cursor = conn.cursor()
         cursor.execute("""
-            CREATE TABLE IF NOT EXISTS transcricoes (
-                id SERIAL PRIMARY KEY,
-                audio_filename VARCHAR(255) UNIQUE,
-                transcription TEXT,
-                from_field VARCHAR(255),
-                to_field VARCHAR(255),
-                timestamp TIMESTAMP
-            );
-        """)
+        CREATE TABLE IF NOT EXISTS transcricoes (
+            hash_arquivo TEXT PRIMARY KEY,
+            transcricao TEXT,
+            from_field TEXT,
+            to_field TEXT,
+            timestamp TIMESTAMP
+        );
+    """)
         conn.commit()
         cursor.close()
         conn.close()
         #print("Tabela 'transcricoes' criada ou já existe.")
-    except sqlite3.Error as e: #substituído psycopg2.Error
+    except psycopg2.Error as e:
         print(f"Erro ao criar a tabela: {e}")
         raise
 
 
 # Função para salvar uma nova transcrição
-def salvar_transcricao(audio_filename, transcription, from_field, to_field, timestamp):
+def salvar_transcricao(hash_arquivo, transcricao, from_field, to_field, timestamp):
     try:
-        #conn = conectar()
-        conn = conn = sqlite3.connect('transcricoes_db.db')
+        conn = conectar()
         cursor = conn.cursor()
 
         # Verificar os dados a serem inseridos
-        print(f"Inserindo no banco: filename={audio_filename}, transcription={transcription[:30]}...")
+        #print(f"Inserindo no banco: filename={audio_filename}, transcription={transcription[:30]}...")
 
-        cursor.execute("""
-            INSERT INTO transcricoes (audio_filename, transcription, from_field, to_field, timestamp)
-            VALUES (?, ?, ?, ?, ?);
-        """, (audio_filename, transcription, from_field, to_field, timestamp)) # substituído %s por ?
+        cursor.execute(
+        """
+        INSERT INTO transcricoes (hash_arquivo, transcricao, from_field, to_field, timestamp)
+        VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT (hash_arquivo) DO NOTHING
+        """,
+        (hash_arquivo, transcricao, from_field, to_field, timestamp)
+    )
         conn.commit()
 
-        print(f"Transcrição salva no banco de dados: {audio_filename}")
+        #print(f"Transcrição salva no banco de dados: {audio_filename}")
         cursor.close()
         conn.close()
 
-    except sqlite3.Error as e: #substituído psycopg2.Error
+    except psycopg2.Error as e:
         print(f"Erro ao salvar transcrição no banco de dados: {e}")
         raise
 
 
 
 # Função para buscar uma transcrição já existente
-def buscar_transcricao(audio_filename):
+def buscar_transcricao(hash_arquivo):
     try:
-        #conn = conectar()
-        conn = conn = sqlite3.connect('transcricoes_db.db')
+        conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("SELECT transcription FROM transcricoes WHERE audio_filename = ?", (audio_filename,)) # substituído %s por ?
+        cursor.execute("SELECT transcricao FROM transcricoes WHERE hash_arquivo = %s", (hash_arquivo,))
         result = cursor.fetchone()
         cursor.close()
         conn.close()
         return result[0] if result else None
-    except sqlite3.Error as e: #substituído psycopg2.Error
+    except psycopg2.Error as e:
         print(f"Erro ao buscar transcrição no banco de dados: {e}")
         raise
